984403c26175065f4f73c9bba8edd20319a50c99
Irandi Silva <167270911+Irandisilvaa@users.noreply.github.com>
2024-08-09 00:15:43 -0300

    Update sistemadeimpressao.c

--- sistemadeimpressao.c
+++ sistemadeimpressao.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*A empresa de tecnologia Poxim Tech está
+desenvolvendo um sistema de impressão
+centralizado para otimizar a utilização das
+impressoras e reduzir os custos com manutenção e
+reposição de suprimentos
+▶ Todos os documentos enviados para impressão são
+organizados por ordem de chegada, sendo
+despachados para uma impressora que estiver ociosa
+▶ Os nomes dos arquivos e das impressoras possuem até
+50 caracteres, sendo limitados a letras e números
+▶ A quantidade de páginas do documento determina
+quanto tempo será utilizado na impressora alocada,
+assumindo que todas as impressoras possuem a
+mesma velocidade de impressão
+▶ Após cada impressão ser concluída, as folhas
+impressas de todas as impressoras são
+automaticamente recolhidas e empilhadas para
+serem entregues;
+Formato do arquivo de entrada
+▶ [#n]
+▶ [Impressora 1]
+▶ · · ·
+▶ [Impressora n]
+▶ [#m]
+▶ [Documento 1] [#Paginas ´ 1]
+▶ · · ·
+▶ [Documento m] [#Paginas m ´ ]
+*/
 // Estrutura para representar um documento
 typedef struct no {
     char nomeArquivo[51];
